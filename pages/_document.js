a335acc02cf90ff2bb51f98caf4e954c59b3ff28
anneelv <anneelvira.lauw@gmail.com>
2023-06-30 21:24:37 +1000

    Change web icon

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@500;700;800&display=swap" rel="stylesheet" />
+
+        {/* WEB ICON */}
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
       </Head>
       <body>
         <Main />
